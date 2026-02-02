eaaf5717e33684f39c743fa32b557e25fbcc2662
ivangordienko2004-beep <ivangordienko2004@gmail.com>
2026-02-02 14:57:26 +0300

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,7 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  darkMode: 'class',                  // важно
+  content: ["./index.html", "./src/**/*.{ts,tsx}"],
+  theme: { extend: {} },
+  plugins: [],
+}
